7a67e9106792bc824cc01602240dc4045072ee96
DhanrajNaik <dhanrajnaik522@gmail.com>
2015-06-27 16:10:12 +0530

    Initial commit hehhhe

---
+++ app/src/main/java/com/example/systems_5/toolbar_dj/Tab1.java
@@ -0,0 +1,22 @@
+package com.example.systems_5.toolbar_dj;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+/**
+ * Created by System S-5 on 27-06-2015.
+ */
+public class Tab1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab1, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/systems_5/toolbar_dj/Tab3.java
@@ -0,0 +1,20 @@
+package com.example.systems_5.toolbar_dj;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by System S-5 on 27-06-2015.
+ */
+public class Tab3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab3,container,false);
+        return v;
+    }
+}
